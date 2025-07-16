4290d661fba0e6ae1a0158cd39f8195ed1c2c285
Pedro Henrique Oliveira <pedro.cpt@hotmail.com>
2025-07-16 09:37:41 -0300

    feature/ banner e header

--- frontend/site-ingressos/src/app/components/banner/banner.component.ts
+++ frontend/site-ingressos/src/app/components/banner/banner.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,6 +8,37 @@ import { CommonModule } from '@angular/common';
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.css']
 })
-export class BannerComponent {
+export class BannerComponent implements OnInit, OnDestroy {
+  imagens = [
+    'assets/banner-maiara-e-maraisa.webp',
+    'assets/banner-marcos-e-belutti.webp',
+    'assets/banner-simplifica-festival.webp'
+  ];
 
+  slideAtual = 0;
+  intervalo: any;
+
+  ngOnInit(): void {
+    this.iniciarCarrossel();
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.intervalo);
+  }
+
+  iniciarCarrossel() {
+    this.intervalo = setInterval(() => this.proximo(), 2500); // muda a cada 1,5 segundo 
+  }
+
+  proximo() {
+    this.slideAtual = (this.slideAtual + 1) % this.imagens.length;
+  }
+
+  anterior() {
+    this.slideAtual = (this.slideAtual - 1 + this.imagens.length) % this.imagens.length;
+  }
+
+  irParaSlide(index: number){
+    this.slideAtual = index;
+  }
 }
